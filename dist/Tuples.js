cfc0c6c591d1e72baac5f205241a12e7f3066e3d
alamin khan <alaminuixd@gmail.com>
2025-06-06 03:13:21 +0600

    before tuples

---
+++ dist/Tuples.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.person = void 0;
+exports.person = [
+    "one",
+    23,
+    true,
+    { name: "khan" },
+];
+console.log(exports.person);
---
+++ src/Tuples.ts
@@ -0,0 +1,7 @@
+export const person: [string, number, boolean, {}] = [
+  "one",
+  23,
+  true,
+  { name: "khan" },
+];
+console.log(person);
