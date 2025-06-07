26c9a82e22f0e4cb13aa026308984bcde825e5a0
alamin khan <alaminuixd@gmail.com>
2025-06-08 01:29:28 +0600

    interface

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,12 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const uuid_1 = require("uuid");
-const author = {
-    id: (0, uuid_1.v4)(),
-    name: "Al Amin khan",
-    age: 39,
-    avatar: "img/avater.png",
+const author1 = {
+    name: "Al Amin",
+    avater: "img/avater.png",
+    age: 34,
+    father: "Syed Ahmed",
 };
-for (let key in author) {
-    console.log(`${key}: ${author[key]}`);
+for (const key of Object.keys(author1)) {
+    // console.log(`${key}: ${author1[key]}`);
 }
+const newPost2 = {
+    title: "My first Blog Post",
+    body: "Lets play something interesting and learn.",
+    tags: ["gaming", "educational"],
+    create_at: new Date(),
+    author: {
+        name: "Jhon Doe",
+        avater: "img/avater.png",
+        age: 34,
+        father: "Syed Ahmed",
+    },
+};
+const newPost3 = {
+    title: "My Second Blog Post",
+    body: "When I was a young boy, I used to like playing football.",
+    tags: ["gaming", "educational"],
+    create_at: new Date(),
+    author: author1,
+};
+function createPost(post) {
+    const { title, body, tags, create_at, author } = post;
+    const { name, avater, age, father } = author;
+    console.log(`Created post ${title} by ${name}`);
+}
+createPost(newPost2);
+let post2 = [];
+post2.push(newPost2);
+post2.push(newPost3);
+console.log(post2);
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,18 +1,60 @@
 import { v4 as uuidv4 } from "uuid";
+
 interface Author {
-  id: string;
   name: string;
+  avater: string;
   age: number;
-  avatar: string;
+  father: string;
 }
 
-const author: Author = {
-  id: uuidv4(),
-  name: "Al Amin khan",
-  age: 39,
-  avatar: "img/avater.png",
+const author1: Author = {
+  name: "Al Amin",
+  avater: "img/avater.png",
+  age: 34,
+  father: "Syed Ahmed",
 };
 
-for (let key in author) {
-  console.log(`${key}: ${author[key]}`);
+for (const key of Object.keys(author1) as (keyof Author)[]) {
+  // console.log(`${key}: ${author1[key]}`);
 }
+
+interface Post {
+  title: string;
+  body: string;
+  tags: string[];
+  create_at: Date;
+  author: Author;
+}
+
+const newPost2: Post = {
+  title: "My first Blog Post",
+  body: "Lets play something interesting and learn.",
+  tags: ["gaming", "educational"],
+  create_at: new Date(),
+  author: {
+    name: "Jhon Doe",
+    avater: "img/avater.png",
+    age: 34,
+    father: "Syed Ahmed",
+  },
+};
+
+const newPost3: Post = {
+  title: "My Second Blog Post",
+  body: "When I was a young boy, I used to like playing football.",
+  tags: ["gaming", "educational"],
+  create_at: new Date(),
+  author: author1,
+};
+
+function createPost(post: Post) {
+  const { title, body, tags, create_at, author } = post;
+  const { name, avater, age, father } = author;
+  console.log(`Created post ${title} by ${name}`);
+}
+createPost(newPost2);
+
+let post2: Post[] = [];
+post2.push(newPost2);
+post2.push(newPost3);
+console.log(post2);
